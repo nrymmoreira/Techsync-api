dc405ea6ce1625b9bbec86f35247e08ab04bccd6
Matheus Laborde <mlabordebr@gmail.com>
2025-06-14 22:05:45 -0300

    Proteção de endpoints do usuario com autenticação exigindo o token JWT no header

---
+++ src/main/java/br/com/techsync/security/JwtFilter.java
@@ -0,0 +1,66 @@
+package br.com.techsync.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    // Usar a mesma chave secreta que no arquivo JwtUtil
+    private final String chaveSecreta = "MinhaChaveSuperSecreta1234567890123456";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7); // Remove o "Bearer "
+
+            try {
+                // Valida o token
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(chaveSecreta.getBytes())
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String email = claims.getSubject();
+                request.setAttribute("email", email);
+
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token JWT inválido ou expirado.");
+                return;
+            }
+
+        } else {
+            if (isProtectedEndpoint(request)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token JWT ausente.");
+                return;
+            }
+        }
+
+        // Continua a requisição
+        filterChain.doFilter(request, response);
+    }
+
+    // Define quais endpoints precisam de autenticação JWT
+    private boolean isProtectedEndpoint(HttpServletRequest request) {
+        String path = request.getRequestURI();
+
+        // Protege as rotas acessadas que fica /api/usuarios exceto login
+        return path.startsWith("/api/usuarios") && !path.endsWith("/login");
+    }
+}
+
